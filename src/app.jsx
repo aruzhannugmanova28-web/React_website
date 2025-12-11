ba26836bdc045faa22e3243d55546c224aa4405a
Aruzhan Nugmanova <aruzhan.nugmanova@mac.milton.edu>
2025-12-10 21:25:34 -0500

    added text

--- src/app.jsx
+++ src/app.jsx
@@ -1,43 +1,19 @@
 import { useState } from 'preact/hooks'
 import preactLogo from './assets/preact.svg'
 import viteLogo from '/vite.svg'
-import './app.css'
+import './app.css';
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.jsx</code> and save to test HMR
-        </p>
-      </div>
       <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
+        Have you read{' '}
+        <a href="https://en.wikipedia.org/wiki/Odyssey" target="_blank">
+          The Odyssey
         </a>
-        , the official Preact + Vite starter
+        , one of the most significant works of the Western canon?
       </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+      <p>Let’s dive into the ancient Greek world full of drama and hidden motifs…</p>
     </>
-  )
+  );
 }
